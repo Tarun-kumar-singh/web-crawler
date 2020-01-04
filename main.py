54cbf42f6e70707400e1d5f0b67a728434b620d4
Tarun kumar singh <tarunks2512@gmail.com>
2020-01-05 00:39:35 +0530

    added multi threaded to crawl entire web site

--- main.py
+++ main.py
@@ -7,5 +7,38 @@ from general import *
 
 PROJECT_NAME = ''
 HOMEPAGE = ''
+NO_OF_THREADS = 8
+QUEUE_FILE = PROJECT_NAME + '/queue.txt'
+CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
+queue = Queue()
 DOMAIN_NAME = get_domian_name(HOMEPAGE)
 Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
+
+
+def create_workers():
+    for _ in range(NO_OF_THREADS):
+        t = threading.Thread(target=work)
+        t.daemon
+        t.start()
+
+def work():
+    while True:
+        url = queue.get()
+        Spider.crawl_page(threading.current_thread().name, url)
+        queue.task_done()
+
+def create_job():
+    for link in file_to_set(QUEUE_FILE):
+        queue.put(link)
+    queue.join()
+    crawl()
+
+
+def crawl():
+    queued_links = file_to_set(QUEUE_FILE)
+    if len(queued_links) > 0:
+        create_job()
+
+
+create_workers()
+crawl()
